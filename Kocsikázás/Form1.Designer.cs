14d8623d4092e5afb365a52e1b66a3386b281aa2
kuni02q <70653128+kuni02q@users.noreply.github.com>
2020-09-02 18:55:16 +0200

    Add files via upload

---
+++ Kocsikázás/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Kocsikázás
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.csik7 = new System.Windows.Forms.PictureBox();
+            this.csik8 = new System.Windows.Forms.PictureBox();
+            this.csik9 = new System.Windows.Forms.PictureBox();
+            this.csik10 = new System.Windows.Forms.PictureBox();
+            this.csik11 = new System.Windows.Forms.PictureBox();
+            this.kocsi = new System.Windows.Forms.PictureBox();
+            this.csik5 = new System.Windows.Forms.PictureBox();
+            this.csik4 = new System.Windows.Forms.PictureBox();
+            this.csik3 = new System.Windows.Forms.PictureBox();
+            this.csik2 = new System.Windows.Forms.PictureBox();
+            this.csik1 = new System.Windows.Forms.PictureBox();
+            this.vonal2 = new System.Windows.Forms.PictureBox();
+            this.vonal1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.vonal3 = new System.Windows.Forms.PictureBox();
+            this.csik12 = new System.Windows.Forms.PictureBox();
+            this.csik6 = new System.Windows.Forms.PictureBox();
+            this.ora1 = new System.Windows.Forms.Timer(this.components);
+            this.ellenseg1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.csik7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kocsi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vonal2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vonal1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vonal3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ellenseg1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // csik7
+            // 
+            this.csik7.Location = new System.Drawing.Point(305, 1);
+            this.csik7.Name = "csik7";
+            this.csik7.Size = new System.Drawing.Size(10, 65);
+            this.csik7.TabIndex = 0;
+            this.csik7.TabStop = false;
+            // 
+            // csik8
+            // 
+            this.csik8.Location = new System.Drawing.Point(305, 97);
+            this.csik8.Name = "csik8";
+            this.csik8.Size = new System.Drawing.Size(10, 65);
+            this.csik8.TabIndex = 1;
+            this.csik8.TabStop = false;
+            // 
+            // csik9
+            // 
+            this.csik9.Location = new System.Drawing.Point(305, 193);
+            this.csik9.Name = "csik9";
+            this.csik9.Size = new System.Drawing.Size(10, 65);
+            this.csik9.TabIndex = 2;
+            this.csik9.TabStop = false;
+            // 
+            // csik10
+            // 
+            this.csik10.Location = new System.Drawing.Point(305, 289);
+            this.csik10.Name = "csik10";
+            this.csik10.Size = new System.Drawing.Size(10, 65);
+            this.csik10.TabIndex = 3;
+            this.csik10.TabStop = false;
+            // 
+            // csik11
+            // 
+            this.csik11.Location = new System.Drawing.Point(305, 385);
+            this.csik11.Name = "csik11";
+            this.csik11.Size = new System.Drawing.Size(10, 65);
+            this.csik11.TabIndex = 4;
+            this.csik11.TabStop = false;
+            // 
+            // kocsi
+            // 
+            this.kocsi.BackColor = System.Drawing.Color.Transparent;
+            this.kocsi.Image = ((System.Drawing.Image)(resources.GetObject("kocsi.Image")));
+            this.kocsi.Location = new System.Drawing.Point(189, 411);
+            this.kocsi.Name = "kocsi";
+            this.kocsi.Size = new System.Drawing.Size(42, 80);
+            this.kocsi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kocsi.TabIndex = 5;
+            this.kocsi.TabStop = false;
+            // 
+            // csik5
+            // 
+            this.csik5.Location = new System.Drawing.Point(105, 385);
+            this.csik5.Name = "csik5";
+            this.csik5.Size = new System.Drawing.Size(10, 65);
+            this.csik5.TabIndex = 10;
+            this.csik5.TabStop = false;
+            // 
+            // csik4
+            // 
+            this.csik4.Location = new System.Drawing.Point(105, 289);
+            this.csik4.Name = "csik4";
+            this.csik4.Size = new System.Drawing.Size(10, 65);
+            this.csik4.TabIndex = 9;
+            this.csik4.TabStop = false;
+            // 
+            // csik3
+            // 
+            this.csik3.Location = new System.Drawing.Point(105, 193);
+            this.csik3.Name = "csik3";
+            this.csik3.Size = new System.Drawing.Size(10, 65);
+            this.csik3.TabIndex = 8;
+            this.csik3.TabStop = false;
+            // 
+            // csik2
+            // 
+            this.csik2.Location = new System.Drawing.Point(105, 97);
+            this.csik2.Name = "csik2";
+            this.csik2.Size = new System.Drawing.Size(10, 65);
+            this.csik2.TabIndex = 7;
+            this.csik2.TabStop = false;
+            // 
+            // csik1
+            // 
+            this.csik1.Location = new System.Drawing.Point(105, 1);
+            this.csik1.Name = "csik1";
+            this.csik1.Size = new System.Drawing.Size(10, 65);
+            this.csik1.TabIndex = 6;
+            this.csik1.TabStop = false;
+            // 
+            // vonal2
+            // 
+            this.vonal2.Location = new System.Drawing.Point(205, 1);
+            this.vonal2.Name = "vonal2";
+            this.vonal2.Size = new System.Drawing.Size(10, 525);
+            this.vonal2.TabIndex = 11;
+            this.vonal2.TabStop = false;
+            // 
+            // vonal1
+            // 
+            this.vonal1.Location = new System.Drawing.Point(0, 1);
+            this.vonal1.Name = "vonal1";
+            this.vonal1.Size = new System.Drawing.Size(10, 525);
+            this.vonal1.TabIndex = 12;
+            this.vonal1.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.Location = new System.Drawing.Point(440, 1);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(10, 503);
+            this.pictureBox14.TabIndex = 13;
+            this.pictureBox14.TabStop = false;
+            // 
+            // vonal3
+            // 
+            this.vonal3.Location = new System.Drawing.Point(410, 1);
+            this.vonal3.Name = "vonal3";
+            this.vonal3.Size = new System.Drawing.Size(10, 525);
+            this.vonal3.TabIndex = 14;
+            this.vonal3.TabStop = false;
+            // 
+            // csik12
+            // 
+            this.csik12.Location = new System.Drawing.Point(305, 481);
+            this.csik12.Name = "csik12";
+            this.csik12.Size = new System.Drawing.Size(10, 65);
+            this.csik12.TabIndex = 15;
+            this.csik12.TabStop = false;
+            // 
+            // csik6
+            // 
+            this.csik6.Location = new System.Drawing.Point(105, 481);
+            this.csik6.Name = "csik6";
+            this.csik6.Size = new System.Drawing.Size(10, 65);
+            this.csik6.TabIndex = 16;
+            this.csik6.TabStop = false;
+            // 
+            // ora1
+            // 
+            this.ora1.Enabled = true;
+            this.ora1.Interval = 10;
+            this.ora1.Tick += new System.EventHandler(this.ora1_Tick);
+            // 
+            // ellenseg1
+            // 
+            this.ellenseg1.BackColor = System.Drawing.Color.Transparent;
+            this.ellenseg1.Image = ((System.Drawing.Image)(resources.GetObject("ellenseg1.Image")));
+            this.ellenseg1.Location = new System.Drawing.Point(38, 157);
+            this.ellenseg1.Name = "ellenseg1";
+            this.ellenseg1.Size = new System.Drawing.Size(47, 80);
+            this.ellenseg1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ellenseg1.TabIndex = 17;
+            this.ellenseg1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(422, 503);
+            this.Controls.Add(this.ellenseg1);
+            this.Controls.Add(this.kocsi);
+            this.Controls.Add(this.csik6);
+            this.Controls.Add(this.csik12);
+            this.Controls.Add(this.vonal3);
+            this.Controls.Add(this.pictureBox14);
+            this.Controls.Add(this.vonal1);
+            this.Controls.Add(this.vonal2);
+            this.Controls.Add(this.csik5);
+            this.Controls.Add(this.csik4);
+            this.Controls.Add(this.csik3);
+            this.Controls.Add(this.csik2);
+            this.Controls.Add(this.csik1);
+            this.Controls.Add(this.csik11);
+            this.Controls.Add(this.csik10);
+            this.Controls.Add(this.csik9);
+            this.Controls.Add(this.csik8);
+            this.Controls.Add(this.csik7);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(440, 550);
+            this.MinimumSize = new System.Drawing.Size(440, 550);
+            this.Name = "Form1";
+            this.Text = "Kocsi";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.csik7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kocsi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vonal2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vonal1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vonal3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csik6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ellenseg1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox csik7;
+        private System.Windows.Forms.PictureBox csik8;
+        private System.Windows.Forms.PictureBox csik9;
+        private System.Windows.Forms.PictureBox csik10;
+        private System.Windows.Forms.PictureBox csik11;
+        private System.Windows.Forms.PictureBox kocsi;
+        private System.Windows.Forms.PictureBox csik5;
+        private System.Windows.Forms.PictureBox csik4;
+        private System.Windows.Forms.PictureBox csik3;
+        private System.Windows.Forms.PictureBox csik2;
+        private System.Windows.Forms.PictureBox csik1;
+        private System.Windows.Forms.PictureBox vonal2;
+        private System.Windows.Forms.PictureBox vonal1;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.PictureBox vonal3;
+        private System.Windows.Forms.PictureBox csik12;
+        private System.Windows.Forms.PictureBox csik6;
+        private System.Windows.Forms.Timer ora1;
+        private System.Windows.Forms.PictureBox ellenseg1;
+    }
+}
+
---
+++ Kocsikázás/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kocsikázás
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ora1_Tick(object sender, EventArgs e)
+        {
+            csikMozgas(gyorsasag);
+            ellensegek(gyorsasag + 5);
+        }
+
+        void csikMozgas(int sebesseg)
+        {
+            if (csik1.Top >= 410)
+            {
+                csik1.Top = -50;
+            }
+            else csik1.Top += sebesseg;
+
+            if (csik2.Top >= 410)
+            {
+                csik2.Top = -50;
+            }
+            else csik2.Top += sebesseg;
+
+            if (csik3.Top >= 410)
+            {
+                csik3.Top = -50;
+            }
+            else csik3.Top += sebesseg;
+
+            if (csik4.Top >= 410)
+            {
+                csik4.Top = -50;
+            }
+            else csik4.Top += sebesseg;
+
+            if (csik5.Top >= 410)
+            {
+                csik5.Top = -50;
+            }
+            else csik5.Top += sebesseg;
+
+            if (csik6.Top >= 410)
+            {
+                csik6.Top = -50;
+            }
+            else csik6.Top += sebesseg;
+
+            if (csik7.Top >= 410)
+            {
+                csik7.Top = -50;
+            }
+            else csik7.Top += sebesseg;
+
+            if (csik8.Top >= 410)
+            {
+                csik8.Top = -50;
+            }
+            else csik8.Top += sebesseg;
+
+            if (csik9.Top >= 410)
+            {
+                csik9.Top = -50;
+            }
+            else csik9.Top += sebesseg;
+
+            if (csik10.Top >= 410)
+            {
+                csik10.Top = -50;
+            }
+            else csik10.Top += sebesseg;
+
+            if (csik11.Top >= 410)
+            {
+                csik11.Top = -50;
+            }
+            else csik11.Top += sebesseg;
+
+            if (csik12.Top >= 410)
+            {
+                csik12.Top = -50;
+            }
+            else csik12.Top += sebesseg;
+
+
+        }
+
+        Random vel = new Random();
+        int x;
+        int sav;
+
+        void ellensegek(int sebesseg)
+        {
+            sav = vel.Next(1, 3);
+            if (ellenseg1.Top >= 410)
+            {
+                x = vel.Next(0, 150);
+                ellenseg1.Location = new Point(x, -50);
+            }
+            else ellenseg1.Top += sebesseg;
+        }
+
+        int gyorsasag = 10;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                if (kocsi.Left > 5)
+                {
+                    kocsi.Left += -10;
+                }
+            }
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                if (kocsi.Right < 350 - kocsi.Width)
+                {
+                    kocsi.Left += 10;
+                }
+            }
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                if (gyorsasag < 21)
+                {
+                    gyorsasag++;
+                }
+            }
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                if (gyorsasag > 0)
+                {
+                    gyorsasag--;
+                }
+            }
+
+        }
+    }
+}
